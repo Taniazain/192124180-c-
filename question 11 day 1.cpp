83afa250851073c8c883b96fef81e39730cc0993
Tania <112808747+Taniazain@users.noreply.github.com>
2022-09-23 10:32:44 +0530

    Add files via upload

---
+++ question 11 day 1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+//finding mean of the ungrouped data in array
+float mean(float arr[], int n){
+    float sum = 0;
+
+    for(int i = 0;i < n; i++)
+        sum += arr[i];
+    
+    return sum/n;
+}
+
+//finding median of the ungrouped data in the array
+float median(float arr[], int n){
+
+    //sort the array
+    sort(arr, arr + n);
+
+    if(n % 2 == 0)
+        return (arr[n/2 - 1] + arr[n/2])/2;
+
+    return arr[n/2];
+}
+
+//finding mode of ungrouped data
+float mode( float arr[], int n){
+
+    // Sort the array 
+    sort(arr, arr + n);  
+    int max_count = 1, res = arr[0], count = 1; 
+
+    for (int i = 1; i < n; i++) { 
+        if (arr[i] == arr[i - 1]) 
+            count++; 
+        else { 
+            if (count > max_count) { 
+                max_count = count; 
+                res = arr[i - 1]; 
+            } 
+            count = 1; 
+        } 
+    } 
+    if (count > max_count) 
+    { 
+        max_count = count; 
+        res = arr[n - 1]; 
+    }
